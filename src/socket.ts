48bc7a1439e59f885b4004deff1ac1522fde31d6
ItsZodiaX <itszodiax123@gmail.com>
2023-09-20 23:11:43 +0700

    fix: Type error

--- src/socket.ts
+++ src/socket.ts
@@ -1,19 +1,16 @@
-import { Server, Socket } from "socket.io";
-import express, { Request, Response } from 'express'
-import { uuidv4 } from '@firebase/util'
-import admin from "./Authentication/FirebaseAdmin/admin"; // Adjust the path to point to your Firebase admin setup
+import {Server, Socket} from "socket.io";
+import express, {Request, Response} from 'express'
+import {uuidv4} from '@firebase/util'
 import cookieParser from "cookie-parser";
-import Chat, {IChat} from "./models/chat"
-import User, {IUser} from "./models/user"
-import File, {IFile} from "./models/file"
+import Chat from "./models/chat"
+import User from "./models/user"
+import File from "./models/file"
 import dotenv from 'dotenv';
-import mongoose, { model } from 'mongoose';
+import mongoose from 'mongoose';
 import chalk from 'chalk';
 import ms from 'ms';
 import http from 'http';
 import jwtMiddleware from "./middleware/jwtMiddleware";
-
-
 
 
 dotenv.config();
@@ -22,33 +19,33 @@ const MAX_RETRIES = 10;
 let retries = 0;
 
 const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 5000,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
 };
 
 const connectWithRetry = () => {
-  return mongoose
-    .connect(process.env.DATABASE_URL || '', options)
-    .then(() => {
-      console.log(chalk.greenBright('✔ Connected to database ✔'));
-    })
-    .catch((err) => {
-      console.error(chalk.redBright('✖ Failed to connect to database:'), err);
-      if (retries < MAX_RETRIES) {
-        retries++;
-        const delay = ms('5s');
-        console.warn(
-          chalk.yellow(
-            `⏳ Retry in ${delay / 1000}s... (${retries}/${MAX_RETRIES})`,
-          ),
-        );
-        setTimeout(connectWithRetry, delay);
-      } else {
-        console.error(chalk.bgRed('✖ Max retries reached. Exiting.'));
-        process.exit(1);
-      }
-    });
+    return mongoose
+        .connect(process.env.DATABASE_URL || '', options)
+        .then(() => {
+            console.log(chalk.greenBright('✔ Connected to database ✔'));
+        })
+        .catch((err) => {
+            console.error(chalk.redBright('✖ Failed to connect to database:'), err);
+            if (retries < MAX_RETRIES) {
+                retries++;
+                const delay = ms('5s');
+                console.warn(
+                    chalk.yellow(
+                        `⏳ Retry in ${delay / 1000}s... (${retries}/${MAX_RETRIES})`,
+                    ),
+                );
+                setTimeout(connectWithRetry, delay);
+            } else {
+                console.error(chalk.bgRed('✖ Max retries reached. Exiting.'));
+                process.exit(1);
+            }
+        });
 };
 
 connectWithRetry();
@@ -65,11 +62,11 @@ app.get('/', (req: Request, res: Response) => {
 })
 
 const io = new Server(httpServer, {
-  cors: {
-    origin: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-  },
+    cors: {
+        origin: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        credentials: true,
+    },
 });
 
 const chatNamespace = io.of('/chat');
@@ -103,8 +100,8 @@ const chatNamespace = io.of('/chat');
 
 // chatNamespace.use(authMiddleware);
 io.engine.use(jwtMiddleware);
-export interface File {
-    type: 'file';
+
+interface File {
     name: string;
     fileID: string;
     size: number;
@@ -115,146 +112,142 @@ export interface File {
 }
 
 interface Message {
-    _id: string; 
+    _id: string;
     uid: string;
     username: string;
     content: string | File;
     type: 'file' | 'text';
 }
 
-let cache : Map<string, Message[]> = new Map(); //TODO: Schedule update cached messages to database every one minute
+let cache: Map<string, Message[]> = new Map(); //TODO: Schedule update cached messages to database every one minute
 
 async function SaveCache() {
     try {
-    for (const [chatId] of cache) {
-        await SaveCacheById(chatId)
+        for (const [chatId] of cache) {
+            await SaveCacheById(chatId)
+        }
+        setTimeout(SaveCache, 60 * 1000);
+    } catch (err) {
+        console.log(err)
     }
-    setTimeout(SaveCache, 60*1000);
-  } catch(err) {
-      console.log(err)
-  }
 }
 
 async function SaveCacheById(chatId: string) {
-    try { 
-    const messages = cache.get(chatId) || []
-    if (messages.length == 0) return
-    const data =  messages.map(Message => {
-        return {
-          _id: Message._id,
-          user_id: Message.uid,
-          content: Message.content instanceof File ? Message.content.link : Message.content,
-          type: Message.type
-        }
-    })
-    await Chat.findByIdAndUpdate(chatId, {
-        $addToSet: { messages: data },
-    })
-  } catch (err) {
-      console.log(err)
-  }
+    try {
+        const messages = cache.get(chatId) || []
+        if (messages.length == 0) return
+        const data = messages.map(Message => {
+            return {
+                _id: Message._id,
+                user_id: Message.uid,
+                content: Message.content instanceof File ? Message.content.link : Message.content,
+                type: Message.type
+            }
+        })
+        await Chat.findByIdAndUpdate(chatId, {
+            $addToSet: {messages: data},
+        })
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 SaveCache()
 
 chatNamespace.on('connection', (socket: Socket) => {
-  console.log('User connected to chat:', socket.id, socket.data.user);
+    console.log('User connected to chat:', socket.id, socket.data.user);
 
-  socket.on('join room', async (chatId) => {
-    console.log('User joined room:', chatId)
-    await socket.join(chatId);
-    console.log(chatNamespace.adapter.rooms)
-    if (!cache.has(chatId)) {
-      let idToName = new Map<string, string>()
-      const data = await Chat.findOne({_id: chatId}, {messages: {$slice: -30}})
-        if (data){
-          const pomdata = (await Promise.all(data.messages.map(async message => {
-            if (!idToName.has(message.user_id)) {
-              const user = await User.findById(message.user_id)
-              if (user) {
-                idToName.set(message.user_id, user.username)
-              }
+    socket.on('join room', async (chatId) => {
+        console.log('User joined room:', chatId)
+        await socket.join(chatId);
+        console.log(chatNamespace.adapter.rooms)
+        if (!cache.has(chatId)) {
+            let idToName = new Map<string, string>()
+            const data = await Chat.findOne({_id: chatId}, {messages: {$slice: -30}})
+            if (data) {
+                const messages = (await Promise.all(data.messages.map(async message => {
+                    if (!idToName.has(message.user_id)) {
+                        const user = await User.findById(message.user_id)
+                        if (user) {
+                            idToName.set(message.user_id, user.username)
+                        }
+                    }
+                    const name = idToName.get(message.user_id) || ""
+                    let content: string | File = message.content
+                    if (message.type === "file") {
+                        const file = await File.findById(message.content)
+                        if (file) {
+                            content = {
+                                name: file.name,
+                                fileID: file._id,
+                                size: file.size,
+                                type_file: file.file_type,
+                                lastModified: file.updatedAt?.toString() || "",
+                                link: file.url,
+                                memo: file.memo,
+                            }
+                        }else{
+                            content = "Unknown file"
+                        }
+                    }
+                    return {
+                        _id: message._id,
+                        uid: message.user_id,
+                        username: name,
+                        content: content,
+                        type: message.type,
+                    }
+                }))).filter(message => message !== undefined)
+                cache.set(chatId, messages);
             }
-            const name = idToName.get(message.user_id)
-            if (name){
-              let content : string | File = message.content
-              if (message.type === "file") {
-                const file = await File.findById(message.content)
-                if (file) {
-                  content = {
-                    type: "file",
-                    name: file.name,
-                    fileID: file._id,
-                    size: file.size,
-                    type_file: file.file_type,
-                    lastModified: file.updatedAt?.toString() || "",
-                    link: file.url,
-                    memo: file.memo,
-                  }
-                }
-              }
-              return {
-                _id: message._id,
-                uid: message.user_id,
-                username: name,
-                content: content,
-                type: message.type,
-              }
-            }
-
-          }))).filter(message => message !== undefined)
-          // cache.set(chatId, pomdata); //TODO: Pull 30 recent messages from database
-
         }
-        
-    }
-    socket.emit('room messages', cache.get(chatId)?.slice(-30));
-  });
+        socket.emit('room messages', cache.get(chatId)?.slice(-30));
+    });
 
-  socket.on('leave room', async (chatId) => {
-    await socket.leave(chatId);
-    console.log('User left room:', chatId)
-    if (chatNamespace.adapter.rooms.get(chatId)?.size === 0) {
-        //TODO: Update cached messages to database
-        await SaveCacheById(chatId)
-        cache.delete(chatId);
-    }
-  });
+    socket.on('leave room', async (chatId) => {
+        await socket.leave(chatId);
+        console.log('User left room:', chatId)
+        if (chatNamespace.adapter.rooms.get(chatId)?.size === 0) {
+            //TODO: Update cached messages to database
+            await SaveCacheById(chatId)
+            cache.delete(chatId);
+        }
+    });
 
-  socket.on('send message', (chatId, message) => {
-    console.log(chatNamespace.adapter.rooms)
-    console.log('User sent message:', message)
-    const response : Message = {
-        _id: uuidv4(),
-        uid: "OU3mOuC6dxg1nPtQKq74Ca9H8hx1",
-        //socket.data.user.uid,
-        username: "llllllllllllllllllllllllllllll_l",
-        //socket.data.user.email,
-        content: message,
-        type: 'text',
-    }
-    let messages = cache.get(chatId) || [];
-    messages.push(response);
-    cache.set(chatId, (messages));
-    chatNamespace.to(chatId).emit('receive message', response);
-  });
+    socket.on('send message', (chatId, message) => {
+        console.log(chatNamespace.adapter.rooms)
+        console.log('User sent message:', message)
+        const response: Message = {
+            _id: uuidv4(),
+            uid: "OU3mOuC6dxg1nPtQKq74Ca9H8hx1",
+            //socket.data.user.uid,
+            username: "llllllllllllllllllllllllllllll_l",
+            //socket.data.user.email,
+            content: message,
+            type: 'text',
+        }
+        let messages = cache.get(chatId) || [];
+        messages.push(response);
+        cache.set(chatId, (messages));
+        chatNamespace.to(chatId).emit('receive message', response);
+    });
 
-  socket.on('request messages', (chatId, timestamp) => {
-    //TODO: Pull other 30 messages from database that have timestamp less than the timestamp received
-    //      then cache and send them back to the client
-  });
+    socket.on('request messages', (chatId, timestamp) => {
+        //TODO: Pull other 30 messages from database that have timestamp less than the timestamp received
+        //      then cache and send them back to the client
+    });
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected from chat:', socket.id);
-  });
+    socket.on('disconnect', () => {
+        console.log('User disconnected from chat:', socket.id);
+    });
 });
 
 httpServer.listen(PORT, "0.0.0.0", () => {
-  console.log(
-    chalk.greenBright.bold(
-      `✔ ServerSocket started at ${chalk.underline.white(
-        `http://localhost:${PORT}`,
-      )}`,
-    ),
-  )
+    console.log(
+        chalk.greenBright.bold(
+            `✔ ServerSocket started at ${chalk.underline.white(
+                `http://localhost:${PORT}`,
+            )}`,
+        ),
+    )
 });
